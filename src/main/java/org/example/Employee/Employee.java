fbcf75915536bac7d5dcf4cc0eb8f252164d8026
kaarakin <taigr@list.ru>
2026-01-06 14:32:52 +0300

    feat(Employee): добавлен интерфейс Employee

---
+++ src/main/java/org/example/Employee/Employee.java
@@ -0,0 +1,8 @@
+package org.example.Employee;
+
+// Интерфейс Employee предоставляет интерфейс работника зоопарка
+public interface Employee {
+    String getName();
+    String getPosition();
+    void performDuties();
+}
