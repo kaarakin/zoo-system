4ca9aa14cd4164a1f9e9e919cf625b3a6c961d6d
kaarakin <taigr@list.ru>
2026-01-06 14:31:00 +0300

    feat(Animal): добавлен интерфейс Animal

---
+++ src/main/java/org/example/Animal/Animal.java
@@ -0,0 +1,9 @@
+package org.example.Animal;
+
+// Интерфейс Animal предоставляет интерфейс животного
+public interface Animal {
+    String getName();
+    String getSpecies();
+    String makeSound();
+    String getHabitatType();
+}
