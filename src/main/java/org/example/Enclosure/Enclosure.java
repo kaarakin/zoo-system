65314448ab7725854b557b3bb02619d227628132
kaarakin <taigr@list.ru>
2026-01-06 16:42:18 +0300

    feat(Enclosure): добавлены методы в Enclosure

--- src/main/java/org/example/Enclosure/Enclosure.java
+++ src/main/java/org/example/Enclosure/Enclosure.java
@@ -1,5 +1,6 @@
 package org.example.Enclosure;
 import org.example.Animal.Animal;
+import org.example.Employee.Observer;
 import java.util.List;
 
 // Интерфейс Enclosure предоставляет интерфейс вольера
@@ -7,6 +8,8 @@ public interface Enclosure {
     String getCode();
     double getArea();
     String getType();
+    void addObserver(Observer observer);
+    void removeObserver(Observer observer);
     boolean addAnimal(Animal animal);
     boolean removeAnimal(Animal animal);
     List<Animal> getAnimals();
