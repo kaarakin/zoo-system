69ddc9b0b55ad0f5ce5658c77398a198ed43b4ed
kaarakin <taigr@list.ru>
2026-01-06 15:42:16 +0300

    feat(Employee): добавлен интерфейс Observer

---
+++ src/main/java/org/example/Employee/Observer.java
@@ -0,0 +1,7 @@
+package org.example.Employee;
+
+// Интерфейс Observer представляет Наблюдателя
+public interface Observer {
+    // Метод update позволяет получить обновление
+    void update(String message);
+}
